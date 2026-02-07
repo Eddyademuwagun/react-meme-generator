d1da1e070c5e3523422160cf2f28e87b583dd87b
Eddyademuwagun <ademuwaguneddy@gmail.com>
2026-02-07 22:21:16 +0100

    Implement download

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ export default function App() {
 
   return (
     <div className="App">
-      <label>
+      <label className="label">
         meme template
         <input onChange={(e) => setMemeTemplate(e.target.value)} type="text" />
       </label>
@@ -22,16 +22,17 @@ export default function App() {
         <input onChange={(e) => setTopText(e.target.value)} type="text" />
       </label>
       <br></br> {/* bottom label to be under the top lable*/}
-      <label>
-        bottom text
-        <input onChange={(e) => setBottomText(e.target.value)} type="text" />
-      </label>
       <br></br>
       <img
         src={`https://api.memegen.link/images/${memeTemplate}/${topText}/${bottomText}.webp`}
         data-test-id="meme-image"
         alt="Generated meme"
       />
+      <label>
+        <br></br>
+        bottom text
+        <input onChange={(e) => setBottomText(e.target.value)} type="text" />
+      </label>
     </div>
   );
 }
